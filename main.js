a614790c0011be7375cded73acbcf1e087693ccf
Urbaczka ivan <ivanneurbaczka@gmail.com>
2019-01-06 10:51:57 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const input = document.querySelector('input');
+
+input.addEventListener('focus',function(){
+    input.style.background = "#C0FFEE";
+})
+
+
+input.addEventListener('blur',function(){
+    input.style.background = "white";
+})
